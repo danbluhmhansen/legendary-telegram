2c3317d42569f49b25720b78486ac9e6e263a2cc
danbluhmhansen <danhansen@me.com>
2021-11-27 17:54:29 +0100

    .

---
+++ Tests/Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+using Json.Logic;
+using Json.Schema;
+
+JsonDocument doc = JsonDocument.Parse("{\"Strength\":8}");
+
+Rule? rule = JsonSerializer.Deserialize<Rule>("{\"+\":[{\"var\":\"Strength\"},2]}");
+
+if (rule is not null)
+{
+	JsonElement result = rule.Apply(doc.RootElement);
+	Console.WriteLine(result.GetRawText());
+}
