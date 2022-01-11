dc5b052e7f02914255b6e996ab51abca95a71a92
danbluhmhansen <16237377+danbluhmhansen@users.noreply.github.com>
2022-01-11 21:08:10 +0100

    Added DataServiceQuery properties for all models.

--- Source/Client/Data/ODataServiceContext.cs
+++ Source/Client/Data/ODataServiceContext.cs
@@ -11,7 +11,16 @@ public class ODataServiceContext : DataServiceContext
 	public ODataServiceContext(Uri serviceRoot) : base(serviceRoot)
 	{
 		this.Characters = base.CreateQuery<Character>("Characters");
+		this.Features = base.CreateQuery<Feature>("Features");
+		this.CoreEffects = base.CreateQuery<CoreEffect>("CoreEffects");
+		this.Effects = base.CreateQuery<Effect>("Effects");
+		this.CharacterFeatures = base.CreateQuery<CharacterFeature>("CharacterFeatures");
 	}
 
 	public DataServiceQuery<Character> Characters { get; }
+	public DataServiceQuery<Feature> Features { get; }
+	public DataServiceQuery<CoreEffect> CoreEffects { get; }
+	public DataServiceQuery<Effect> Effects { get; }
+
+	public DataServiceQuery<CharacterFeature> CharacterFeatures { get; }
 }
